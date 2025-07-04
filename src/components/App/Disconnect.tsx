84db089ff94956f9134fd4a857386a8251049dc3
Nikolai <nikolai1973@gmail.com>
2025-07-04 16:47:30 +0300

    disconnect fixed

--- src/components/App/Disconnect.tsx
+++ src/components/App/Disconnect.tsx
@@ -5,17 +5,20 @@ import { useDisconnect } from "wagmi";
 import { useXMTP } from "@/contexts/XMTPContext";
 import { useSettings } from "@/hooks/useSettings";
 import { CenteredLayout } from "@/layouts/CenteredLayout";
+import { usePrivy } from "@privy-io/react-auth";
 
 export const Disconnect: React.FC = () => {
   const navigate = useNavigate();
   const { disconnect } = useDisconnect();
   const { setEphemeralAccountEnabled, ephemeralAccountEnabled } = useSettings();
   const { disconnect: disconnectClient } = useXMTP();
+  const { logout } = usePrivy();
 
   useEffect(() => {
     if (ephemeralAccountEnabled) {
       setEphemeralAccountEnabled(false);
     }
+    logout();
     disconnect(undefined, {
       onSuccess: () => {
         disconnectClient();
